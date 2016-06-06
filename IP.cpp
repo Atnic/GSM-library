c20c06b39c442a44232dc0287f2e9a73128a298f
Farid Inawan <frdteknikelektro@gmail.com>
2016-06-06 11:07:10 +0700

    Add Documentation on IP Class

--- IP.cpp
+++ IP.cpp
@@ -1,12 +1,5 @@
 #include "IP.h"
 
-/* IP Class */
-IP::IP(DTE &dte, GPRS &gprs)
-{
-	this->dte = &dte;
-	this->gprs = &gprs;
-}
-
 bool IP::atBearerSettings(unsigned char cmdType, unsigned char cid, const char paramTag[], const char paramValue[]) {
 	char buffer[22 + strlen(paramTag) + strlen(paramValue)];  // "AT+SAPBR=X,X,\"{paramTag}\",\"{paramValue}\"\r\r\n"
 	struct BearerProfile bearerProfile;
@@ -22,7 +15,7 @@ bool IP::atBearerSettings(unsigned char cmdType, unsigned char cid, const char p
 	else
 		return false;
 
-  __FlashStringHelper *response = F("+SAPBR: ");
+  const __FlashStringHelper *response = F("+SAPBR: ");
 
   bearerProfile = this->bearerProfile[cid-1];
 	dte->clearReceivedBuffer();
@@ -45,36 +38,73 @@ bool IP::atBearerSettings(unsigned char cmdType, unsigned char cid, const char p
 		for (unsigned char i = 0; i < 6; i++) {
 			if(!dte->ATResponse()) return false;
 			if(dte->isResponseContain("CONTYPE: ")) {
-				response = F("CONTYPE: ");
+				const __FlashStringHelper *response = F("CONTYPE: ");
 				char *pointer = strstr_P(dte->getResponse(), (const char *)response) + strlen_P((const char *)response);
 				char *str = strtok(pointer, ",\"");
 				strcpy(bearerProfile.connParam.contype, str);
 			}
 			else if (dte->isResponseContain("APN: ")) {
-				response = F("APN: ");
+				const __FlashStringHelper *response = F("APN: ");
 				char *pointer = strstr_P(dte->getResponse(), (const char *)response) + strlen_P((const char *)response);
 				char *str = strtok(pointer, ",\"");
 				strcpy(bearerProfile.connParam.apn, str);
 			}
-			else if (dte->isResponseContain("APN: ")) {
-				response = F("APN: ");
+			else if (dte->isResponseContain("USER: ")) {
+				const __FlashStringHelper *response = F("USER: ");
 				char *pointer = strstr_P(dte->getResponse(), (const char *)response) + strlen_P((const char *)response);
 				char *str = strtok(pointer, ",\"");
-				strcpy(bearerProfile.connParam.apn, str);
+				strcpy(bearerProfile.connParam.user, str);
+			}
+			else if (dte->isResponseContain("PWD: ")) {
+				const __FlashStringHelper *response = F("PWD: ");
+				char *pointer = strstr_P(dte->getResponse(), (const char *)response) + strlen_P((const char *)response);
+				char *str = strtok(pointer, ",\"");
+				strcpy(bearerProfile.connParam.pwd, str);
+			}
+			else if (dte->isResponseContain("PHONENUM: ")) {
+				const __FlashStringHelper *response = F("PHONENUM: ");
+				char *pointer = strstr_P(dte->getResponse(), (const char *)response) + strlen_P((const char *)response);
+				char *str = strtok(pointer, ",\"");
+				strcpy(bearerProfile.connParam.phonenum, str);
+			}
+			else if (dte->isResponseContain("RATE: ")) {
+				const __FlashStringHelper *response = F("RATE: ");
+				char *pointer = strstr_P(dte->getResponse(), (const char *)response) + strlen_P((const char *)response);
+				char *str = strtok(pointer, ",\"");
+				bearerProfile.connParam.rate = str[0] - '0';
 			}
 		}
 		if(!dte->ATResponseOk()) return false;
-		bearerProfile[cid-1] = bearerProfile;
+		this->bearerProfile[cid-1] = bearerProfile;
 	}
-	else if(!dte->ATResponseOk(5000)) return false;
+	else if(!dte->ATResponseOk(10000)) return false;
 	return true;
 }
 
+/* IP Class */
+IP::IP(DTE &dte, GPRS &gprs)
+{
+	this->dte = &dte;
+	this->gprs = &gprs;
+}
+
 void IP::setConnectionParamGprs(const char apn[], const char user[], const char pwd[], unsigned char cid) {
-	atBearerSettings(3, cid, "APN", apn);
-	atBearerSettings(3, cid, "USER", user);
-	atBearerSettings(3, cid, "PWD", pwd);
-	atBearerSettings(4, cid);
+	struct ConnParam connParam = this->bearerProfile[cid-1].connParam;
+	bool change = false;
+
+	if (strcmp(connParam.apn, apn) != 0) {
+		atBearerSettings(3, cid, "APN", apn);
+		change = true;
+	}
+	if (strcmp(connParam.user, user) != 0) {
+		atBearerSettings(3, cid, "USER", user);
+		change = true;
+	}
+	if (strcmp(connParam.pwd, pwd) != 0) {
+		atBearerSettings(3, cid, "PWD", pwd);
+		change = true;
+	}
+	if (change) getConnectionParam(cid);
 }
 
 struct ConnStatus IP::getConnectionStatus(unsigned char cid) {
@@ -85,4 +115,27 @@ struct ConnStatus IP::getConnectionStatus(unsigned char cid) {
 struct ConnParam IP::getConnectionParam(unsigned char cid) {
 	atBearerSettings(4, cid);
 	return bearerProfile[cid-1].connParam;
+}
+
+bool IP::openConnection(unsigned char cid) {
+	struct ConnStatus connStatus= getConnectionStatus(cid);
+
+	if (connStatus.status == 3) {
+		if (!gprs->isAttached()) return false;
+		if (!atBearerSettings(1, cid)) return false;
+		connStatus = getConnectionStatus(cid);
+	}
+	if (connStatus.status >= 2) return false;
+	return true;
+}
+
+bool IP::closeConnection(unsigned char cid) {
+	struct ConnStatus connStatus= getConnectionStatus(cid);
+
+	if (connStatus.status == 1) {
+		if (!atBearerSettings(0, cid)) return false;
+		connStatus = getConnectionStatus(cid);
+	}
+	if (connStatus.status <= 1) return false;
+	return true;
 }
--- IP.h
+++ IP.h
@@ -1,6 +1,3 @@
-/*
-*
-*/
 #ifndef IP_h
 #define IP_h
 
@@ -35,15 +32,68 @@ private:
   GPRS *gprs;
   struct BearerProfile bearerProfile[3];
 
+  /**
+   * Command AT+SABPR
+   * @param  cmdType    Command type 1 - 5
+   *                    0: Close bearer
+   *                    1: Open bearer
+   *                    2: Query bearer
+   *                    3: Set bearer parameter
+   *                    4: Get bearer parameter
+   *                    5: Save the value of parameter to NVRAM
+   * @param  cid        Context Identifier
+   * @param  paramTag   Parameter Tag for command type 3
+   *                    "CONTYPE": GPRS or CSD
+   *                    "APN": Access Point Name
+   *                    "USER": User Name
+   *                    "PWD": Password
+   *                    "PHONENUM": Phone Number for CSD Call
+   *                    "CONRATE": CSD Connection Rate
+   * @param  paramValue Parameter Value
+   * @return            true: If command successful, false: Otherwise
+   */
   bool atBearerSettings(unsigned char cmdType, unsigned char cid, const char paramTag[] = "", const char paramValue[] = "");
 
 public:
   IP(DTE &dte, GPRS &gprs);
 
-  void setConnectionParamGprs(const char apn[], const char user[], const char pwd[]);
+  /**
+   * Set GPRS Connection Parameter.
+   * @param apn  Access Point Name
+   * @param user User Name, default: No username
+   * @param pwd  Password, default: No password
+   * @param cid  Context Identifier, default: 1
+   */
+  void setConnectionParamGprs(const char apn[], const char user[] = "", const char pwd[] = "", unsigned char cid = 1);
 
+  /**
+   * Get Connection status in Struct ConnStatus.
+   * @param  cid Context identifier, default: 1
+   * @return     ConnStatus Struct
+   */
   struct ConnStatus getConnectionStatus(unsigned char cid = 1);
+
+  /**
+   * Get Connection parameter in Struct ConnParam.
+   * Connection parameter can be save on 3 storage, which is identified by CID
+   * @param  cid Context Identifier, default: 1
+   * @return     ConnParam Struct
+   */
   struct ConnParam getConnectionParam(unsigned char cid = 1);
+
+  /**
+   * Open connection, register to network, get IP Adresses
+   * @param  cid Context identifier, default: 1
+   * @return     true: If success, false: Otherwise
+   */
+  bool openConnection(unsigned char cid = 1);
+
+  /**
+   * Close connection, unregister to network.
+   * @param  cid Context identifier, default: 1
+   * @return     true: If success, false: Otherwise
+   */
+  bool closeConnection(unsigned char cid = 1);
 };
 
 #endif
