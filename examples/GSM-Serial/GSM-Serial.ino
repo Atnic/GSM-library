068abdc5bddc5074eab98372d646ff949b4b6c7b
Farid Inawan <frdteknikelektro@gmail.com>
2016-06-11 12:39:10 +0700

    Rewrite Example

--- examples/GSM-Serial/GSM-Serial.ino
+++ examples/GSM-Serial/GSM-Serial.ino
@@ -1,10 +1,18 @@
 #include <SoftwareSerial.h>
+#include <DTE.h>
+
+#define PINPOWER 9
+#define DEBUG true
 
 SoftwareSerial SSerial(10, 11);
+DTE dte(SSerial, PINPOWER, DEBUG);
 
 void setup(void) {
   Serial.begin(9600);
-  SSerial.begin(9600);
+  SSerial.begin(19200);
+
+  dte.powerReset();
+  dte.setFlowControl(0, 0); //Turn off FlowControl, because defaults on.
 }
 
 void loop(void) {
--- examples/GsmWebClient/GsmWebClient.ino
+++ examples/GsmWebClient/GsmWebClient.ino
@@ -22,31 +22,36 @@ HTTP http(dte, ip);
 
 void setup() {
   Serial.begin(9600);
-  SSerial.begin(19200);
+  SSerial.begin(19200); // The fastest and yet safe speed for SoftwareSerial
 
+  Serial.println("Power Reset\r\n");
   dte.powerReset();
   ip.setConnectionParamGprs(APN, USERNAME, PASSWORD);
 }
 
 void loop() {
+  if(!dte.AT())
+    dte.powerReset();
+
   Serial.print("Network Registration: ");
   Serial.println(gsm.getNetworkRegistration().status);
   Serial.print("GPRS Attached: ");
   Serial.println(gprs.isAttached());
   Serial.println("Wait to connect...");
   Serial.println();
-  if(ip.openConnection()) {
-    unsigned long timeout = 30;
+  if(ip.openConnection()) { // Open Connection, if it's already connected it just return true
+    Serial.println("Connected!");
+    unsigned long timeout = 30; // The minimum HTTP Timeout setting
     http.initialize(timeout);
     http.action("GET", "arduino.cc/asciilogo.txt");
     unsigned long t = millis();
     while(millis() - t < timeout*1000 && !Urc.httpAction.updated)
-      dte.AT(); // This is necessary, so URC can be captured
+      dte.clearReceivedBuffer(); // This is necessary, so URC can be captured
     if(Urc.httpAction.updated && Urc.httpAction.statusCode == 200) {
       Serial.println("Data Received: ");
 
       for(unsigned long i = 0; i < Urc.httpAction.dataLength;) {
-        char response[51]; // Plus 1 for terminate null char
+        char response[101]; // Plus 1 for terminate null char
         http.readDataReceived(response, sizeof(response)-1, i);
         i += sizeof(response)-1;
         Serial.print(response);
@@ -66,13 +71,19 @@ void loop() {
     else {
       if (Urc.httpAction.updated) {
         Serial.print("Status Code: ");
-        Serial.print(Urc.httpAction.statusCode);
+        Serial.println(Urc.httpAction.statusCode);
       }
       Serial.println("Failed");
     }
     http.terminate();
     Serial.println("Done!");
-    while (true);
+    Serial.println("Press any key to see next connection behave...");
+    Serial.println("*It's already connected, so it's faster. :)");
+    while (Serial.available() == 0);
+    while (Serial.available() > 0) {
+      Serial.read();
+      delay(50);
+    }
   }
-  delay(1000);
+  delay(1000); //It's just for debugging, no need to delay actually
 }
