a30a00f72e92959a7ac7892f49bc95c6d495a4fa
Farid Inawan <frdteknikelektro@gmail.com>
2016-06-12 16:04:41 +0700

    Add GSMLOC Class

---
+++ GSMLOC.cpp
@@ -0,0 +1,46 @@
+#include "GSMLOC.h"
+
+bool GSMLOC::atGSMLocationAndTime(unsigned char type, unsigned char cid) {
+  const __FlashStringHelper *command = F("AT+CIPGSMLOC=%d,%d\r");
+  const __FlashStringHelper *response = F("+CIPGSMLOC: ");
+	char buffer[18]; // "AT+CIPGSMLOC=X,X\r"
+  struct LocationTime locationTime;
+
+	sprintf_P(buffer, (const char *)command, type, cid);
+
+	dte->clearReceivedBuffer();
+  if(!dte->ATCommand(buffer)) return false;
+  if(!dte->ATResponseContain(response, 3000)) return false;
+  char *pointer = strstr_P(dte->getResponse(), (const char *)response) + strlen_P((const char *)response);
+  char *str = strtok(pointer, ",");
+  for (size_t i = 0; i < 5 && str != NULL; i++) {
+    if(i == 0) locationTime.locationCode = str[0] - '0';
+    if(type == 1 && i < 3) {
+      if(i == 1) strcpy(locationTime.longitude, str);
+      if(i == 2) strcpy(locationTime.latitude, str);
+    }
+    if(type == 2 || (type == 1 && i == 3)) {
+      if(i == 1 || i == 3) strcpy(locationTime.date, str);
+      if(i == 2 || i == 4) strcpy(locationTime.time, str);
+    }
+    str = strtok(NULL, ",");
+  }
+	if(!dte->ATResponseOk()) return false;
+  this->locationTime = locationTime;
+	return true;
+}
+
+/* GSMLOC Class */
+GSMLOC::GSMLOC(DTE &dte, IP &ip)
+{
+	this->dte = &dte;
+  this->ip = &ip;
+}
+
+struct LocationTime GSMLOC::getLocationTime(unsigned char cid) {
+  if (strlen(locationTime.longitude) > 0) return locationTime;
+  if (ip->getConnectionStatus(cid).status == 1) {
+    if(!atGSMLocationAndTime(1, cid)) return LocationTime{};
+  }
+  return locationTime;
+}
---
+++ GSMLOC.h
@@ -0,0 +1,43 @@
+#ifndef GSMLOC_h
+#define GSMLOC_h
+
+#include <Arduino.h>
+#include <DTE.h>
+#include <IP.h>
+
+struct LocationTime {
+  unsigned int locationCode = 0;
+  char longitude[12] = "";
+  char latitude[12] = "";
+  char date[11] = "";
+  char time[9] = "";
+};
+
+class GSMLOC
+{
+private:
+  DTE *dte;
+  IP *ip;
+  struct LocationTime locationTime;
+
+  /**
+   * Command AT+GSMLOC=
+   * @param  type Type of command
+   *              1: View location, latitude, and time
+   *              2: View time
+   * @param  cid  Context Identifier, See IP Class, default 1
+   * @return      true: If successful, false: Otherwise
+   */
+  bool atGSMLocationAndTime(unsigned char type, unsigned char cid = 1);
+
+public:
+  GSMLOC(DTE &dte, IP &ip);
+
+  /**
+   * getLocationTime
+   * @return  LocationTime Struct
+   */
+  struct LocationTime getLocationTime(unsigned char cid = 1);
+};
+
+#endif
